3cf1b02a92073a7cafaece48a5020b6b2810666b
natsutsuyu <110134435+natsutsuyu@users.noreply.github.com>
2022-09-21 11:04:41 +0400

    hw4

---
+++ hw4/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace info
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.White;
+            Azerbaijan.Baku aze = new Azerbaijan.Baku(2236000);
+            USA.Washington usa = new USA.Washington(701974);
+            Russia.Moscow rus = new Russia.Moscow(11920000);
+            Console.WriteLine("Baku-" + aze.bakupopulation);
+            Console.WriteLine("Washington-" + usa.washingtonpopulation);
+            Console.WriteLine("Russia-" + rus.moscowpopulation);
+            void compare()
+            {
+                int compareNum;
+                int res = 0;
+                Console.WriteLine($"Choose:\n1.Compare Baku with Moscow\n2.Compare Baku with Washington\n3.Compare Washington with Moscow: ");
+                compareNum = Convert.ToInt32(Console.ReadLine());
+                switch (compareNum)
+                {
+                    case 1:
+                        res = rus.moscowpopulation - aze.bakupopulation;
+                        Console.WriteLine("There are " + res + " more population in Moscow than in Baku.");
+                        break;
+                    case 2:
+                        res = aze.bakupopulation - usa.washingtonpopulation;
+                        Console.WriteLine("There are " + res + " more population in Baku than in Washington.");
+                        break;
+                    case 3:
+                        res = rus.moscowpopulation - usa.washingtonpopulation;
+                        Console.WriteLine("There are " + res + " more population in Moscow than in Washington.");
+                        break;
+                }
+            }
+            compare();
+        }
+    }
+}
+
+
---
+++ hw4/countries.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Azerbaijan
+{
+    class Baku
+    {
+        private int BakuPopulation;
+        public int bakupopulation
+        {
+            get { return BakuPopulation; }
+            set { BakuPopulation = value; }
+        }
+        public Baku(int population)
+        {
+            BakuPopulation = population;
+        }
+    }
+}
+
+namespace USA
+{
+    class Washington
+    {
+        private int WashingtonPopulation;
+        public int washingtonpopulation
+        {
+            get { return WashingtonPopulation; }
+            set { WashingtonPopulation = value; }
+        }
+        public Washington(int population) 
+        { 
+            WashingtonPopulation = population; 
+        }
+    }
+}
+
+namespace Russia
+{
+    class Moscow
+    {
+        private int MoscowPopulation;
+        public int moscowpopulation
+        {
+            get { return MoscowPopulation; }
+            set { MoscowPopulation = value; }
+        }
+        public Moscow(int population)
+        {
+            MoscowPopulation = population;
+        }
+    }
+}
